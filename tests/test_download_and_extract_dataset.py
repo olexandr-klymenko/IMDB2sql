004cc7d965e332ccbf3d18a018829442508f3e53
Oleksandr Klymenko <ok00364@rmn.com>
2018-07-26 17:30:22 +0300

    add download_and_extract_dataset function, cover it by test

---
+++ tests/test_download_and_extract_dataset.py
@@ -0,0 +1,25 @@
+import threading
+import unittest
+from os import remove
+
+from src.utils.get_links import download_and_extract_dataset
+from tests.utils import TestServer, TEST_HTTP_PORT, TEST_TVS_DATA, TEST_FILENAME
+
+
+class TestDownloadAndExtractDataset(unittest.TestCase):
+
+    def setUp(self):
+        self.server = TestServer()
+        thread = threading.Thread(target=self.server.serve_forever)
+        thread.start()
+        self.dataset_path = None
+
+    def tearDown(self):
+        self.server.shutdown()
+        if self.dataset_path:
+            remove(self.dataset_path)
+
+    def test_download_and_extract_dataset(self):
+        dataset_path = download_and_extract_dataset(f'http://127.0.0.1:{TEST_HTTP_PORT}/{TEST_FILENAME}')
+        with open(dataset_path) as f:
+            self.assertEqual(TEST_TVS_DATA, f.read().strip('\n'))
---
+++ tests/utils.py
@@ -0,0 +1,56 @@
+import csv
+import gzip
+import http.server
+import io
+import threading
+
+__all__ = ['TestServer', 'TEST_HTTP_PORT', 'TEST_FILENAME', 'TEST_TVS_DATA']
+
+
+TEST_HTTP_PORT = 8333
+DELIMITER = '\t'
+TEST_FILENAME = 'name.basics.tsv.gz'
+TEST_TVS_DATA = r"""
+nconst	primaryName	birthYear	deathYear	primaryProfession	knownForTitles
+nm0000001	Fred Astaire	1899	1987	soundtrack,actor,miscellaneous	tt0072308,tt0050419,tt0045537,tt0043044
+nm0000002	Lauren Bacall	1924	2014	actress,soundtrack	tt0037382,tt0117057,tt0071877,tt0038355
+nm0000003	Brigitte Bardot	1934	\N	actress,soundtrack,producer	tt0054452,tt0049189,tt0059956,tt0057345
+nm0000004	John Belushi	1949	1982	actor,writer,soundtrack	tt0078723,tt0077975,tt0072562,tt0080455
+nm0000005	Ingmar Bergman	1918	2007	writer,director,actor	tt0060827,tt0050986,tt0050976,tt0083922
+nm0000006	Ingrid Bergman	1915	1982	actress,soundtrack,producer	tt0038787,tt0034583,tt0071877,tt0038109
+nm0000007	Humphrey Bogart	1899	1957	actor,soundtrack,producer	tt0037382,tt0033870,tt0043265,tt0034583
+nm0000008	Marlon Brando	1924	2004	actor,soundtrack,director	tt0047296,tt0068646,tt0070849,tt0078788
+nm0000009	Richard Burton	1925	1984	actor,producer,soundtrack	tt0059749,tt0087803,tt0061184,tt0057877
+""".strip('\n')
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == f'/{TEST_FILENAME}':
+            self._handle_success()
+        else:
+            self.send_error(404)
+        return
+
+    def _handle_success(self):
+        self.send_response(200)
+        self.send_header('Content-Disposition', f'attachment; filename={TEST_FILENAME}')
+        self.send_header('Content-type', 'application/x-gzip')
+        self.end_headers()
+        self.wfile.write(_generate_gzipped_tvs_file_stream())
+
+
+def _generate_gzipped_tvs_file_stream():
+    output = io.StringIO()
+    writer = csv.writer(output, delimiter=DELIMITER)
+    for line in TEST_TVS_DATA.split('\n'):
+        if line.strip():
+            writer.writerow(line.split(DELIMITER))
+    output.seek(0)
+    return gzip.compress(output.read().encode())
+
+
+class TestServer(http.server.HTTPServer):
+    def __init__(self):
+        super().__init__(('', TEST_HTTP_PORT), Handler)
