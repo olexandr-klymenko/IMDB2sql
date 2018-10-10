1ccd2648d6dc3d8d1710e09583372ecdcda95c95
Oleksandr Klymenko <ok00364@rmn.com>
2018-10-10 18:17:00 +0300

    Fix config, implement progress and batch commits

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+import urllib.request
 from os import getcwd
 from os.path import join
 
 from memory_profiler import profile
 
 from src.dal import ImdbDal
-from src.utils import get_config
+from src.utils import get_config, get_links, DataSetsHandler
 
 TITLES_DATASET = 'title.basics.tsv'
 NAMES_DATASET = 'name.basics.tsv'
@@ -19,17 +20,18 @@ DATASET_PATHS = {'title': TITLES_DATASET,
 
 ROOT = '/home/oklymenko/Downloads/IMDB'
 
+
 @profile
 def main():
-    config = get_config(join(getcwd(), 'config', 'datasets.yml'))
-
+    # config = get_config(join(getcwd(), 'config', 'datasets.yml'))
+    #
     # with urllib.request.urlopen(config['data_sets_url']) as response:
     #     imdb_page_content = response.read()
     #
     # handler = DataSetsHandler(get_links(imdb_page_content, config), root=ROOT)
-
+    #
     # handler.download()
-
+    #
     # handler.extract()
 
     dal = ImdbDal(dataset_paths=DATASET_PATHS, root=ROOT)
--- src/dal.py
+++ src/dal.py
@@ -1,7 +1,9 @@
 import csv
 from collections import namedtuple
-from os.path import join
+from os.path import join, getsize, exists
+import os
 import sqlite3
+import sys
 import tempfile
 from typing import Iterable, Iterator, Dict
 
@@ -11,6 +13,8 @@ from sqlalchemy.orm import sessionmaker
 import src.models as models
 
 SQLITE_TYPE = 'sqlite:///'
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
 
 
 class ImdbDal:
@@ -27,6 +31,8 @@ class ImdbDal:
         self.root = root or tempfile.gettempdir()
 
     def db_init(self, db_type=None, db_path=None):
+        if exists(db_path):
+            os.remove(db_path)
         self.db_path = db_path or self.db_path
         self.db_type = db_type or self.db_type
 
@@ -44,17 +50,26 @@ class ImdbDal:
         if self.session is None:
             raise Exception('Database session is not initialized')
         for table_name, dataset_path in self.dataset_paths.items():
-            print(f"Parsing {dataset_path} into '{table_name}' table ...")
-            self._insert_dataset(getattr(self, f'_parse_{table_name}')(join(self.root, dataset_path)))
+            status_line = f"Parsing {dataset_path} into '{table_name}' table ..."
+            self._insert_dataset(getattr(self, f'_parse_{table_name}')(join(self.root, dataset_path)), status_line)
         self.session.commit()
 
-    def _insert_dataset(self, dataset_iter: Iterator):
-        # TODO: implement commiting bunches
-        for line in dataset_iter:
+    def _insert_dataset(self, dataset_iter: Iterator, status_line: str, bunch_size=100000):
+        start_progress = 0
+        print(f'{status_line}: 0%')
+        for idx, (line, progress) in enumerate(dataset_iter):
             self.session.add(line)
+            if progress - start_progress > 0.01:
+                start_progress += 0.01
+                sys.stdout.write(CURSOR_UP_ONE)
+                sys.stdout.write(ERASE_LINE)
+                print(f'{status_line}:{progress:.2f}%')
+
+            if idx % bunch_size == 0:
+                self.session.commit()
 
     def _parse_title(self, dataset_path):
-        for data_set_class in self._parse_dataset(dataset_path):
+        for data_set_class, progress in self._parse_dataset(dataset_path):
             title_line = models.Title(
                 id=getattr(data_set_class, 'tconst'),
                 titleType=getattr(data_set_class, 'titleType'),
@@ -66,10 +81,10 @@ class ImdbDal:
                 runtimeMinutes=getattr(data_set_class, 'runtimeMinutes'),
                 genres=getattr(data_set_class, 'genres'),
             )
-            yield title_line
+            yield title_line, progress
 
     def _parse_principals(self, dataset_path):
-        for data_set_class in self._parse_dataset(dataset_path):
+        for data_set_class, progress in self._parse_dataset(dataset_path):
             principals_line = models.Principals(
                 ordering=getattr(data_set_class, 'ordering'),
                 category=getattr(data_set_class, 'category'),
@@ -78,44 +93,47 @@ class ImdbDal:
                 title=self._get_title(getattr(data_set_class, 'tconst')),
                 name=self._get_name(getattr(data_set_class, 'nconst'))
             )
-            yield principals_line
+            yield principals_line, progress
 
     def _parse_ratings(self, dataset_path):
-        for data_set_class in self._parse_dataset(dataset_path):
+        for data_set_class, progress in self._parse_dataset(dataset_path):
             ratings_line = models.Ratings(
                 averageRating=getattr(data_set_class, 'averageRating'),
                 numVotes=getattr(data_set_class, 'numVotes'),
                 title=self._get_title(getattr(data_set_class, 'tconst'))
             )
-            yield ratings_line
+            yield ratings_line, progress
 
     @staticmethod
     def _parse_dataset(file_path):
+        size = getsize(file_path)
+        read_size = 0
         with open(file_path) as fd:
             tsv_reader = csv.reader(fd, delimiter='\t')
             data_set_class = namedtuple('_', next(tsv_reader))
             for line in tsv_reader:
+                read_size += len(''.join(line)) + len(line)
                 try:
-                    yield data_set_class(*line)
+                    yield data_set_class(*line), (read_size / size) * 100
                 except TypeError:
                     print(f'Invalid line: {line}')
                     continue
-                except Exception as err:
+                except Exception:
                     print(f'Invalid line: {line}')
                     raise
 
     def _parse_name(self, dataset_path):
-        for data_set_class in self._parse_dataset(dataset_path):
+        for data_set_class, progress in self._parse_dataset(dataset_path):
             name_line = models.Name(
                 id=getattr(data_set_class, 'nconst'),
                 primaryName=getattr(data_set_class, 'primaryName'),
                 birthYear=getattr(data_set_class, 'birthYear'),
                 deathYear=self._get_null(getattr(data_set_class, 'deathYear')),
                 primaryProfession=getattr(data_set_class, 'primaryProfession'),
-                titles=self._get_titles(getattr(data_set_class, 'knownForTitles').split(','))
+                titles=self._get_titles(getattr(data_set_class, 'knownForTitles').split(','))  # TODO: optimize
             )
 
-            yield name_line
+            yield name_line, progress
 
     def _get_titles(self, title_ids: Iterable):
         query = self.session.query(models.Title)
